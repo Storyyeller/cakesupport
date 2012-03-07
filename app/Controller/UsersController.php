a71c94ef3959912b57d5451294a3a0f8e22f28b5
Abhishek Shroff <me@shroffabhishek.net>
2012-03-07 01:52:38 -0500

    First stab at login

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -5,12 +5,21 @@ class UsersController extends AppController {
 
   public function login() {
     if ($this->request->is('post')) {
-      #TODO: Login Validation
+      $user = $this->User->findByUsername($this->request->data['User']['username']);
+      $hash = hash("sha256", $this->request->data['User']['password'];
+      if($user && $user['User']['password'] == $hash) {
+        redirect('/');
+      } else {
+        $this->setFlash("Incorrect login attempt");
+      }
     }
   }
 
   public function register() {
     if ($this->request->is('post')) {
+      $this->request->data['User']['password'] = hash("sha256",
+        $this->request->data['User']['password']);
+
       if ($this->User->save($this->request->data)) {
         $this->redirect('/');
       }

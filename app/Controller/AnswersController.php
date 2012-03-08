e1078668f91af93cb1afb6556fd3055677b99075
Abhishek Shroff <me@shroffabhishek.net>
2012-03-07 23:24:12 -0500

    Can now really add comments.

--- app/Controller/AnswersController.php
+++ app/Controller/AnswersController.php
@@ -5,17 +5,17 @@ class AnswersController extends AppController {
   public $uses = array('Question', 'User', 'Answer');
 
   public function post() {
-    if(!$this->Session->read('uid')) {
+    if(!$this->Session->read('User.id')) {
       $this->setFlash('You must be registered to post an answer');
       $this->redirect('/login');
     }
-    $this->request->data['Answer']['user_id'] = $this->Session->read('uid');
+    $this->request->data['Answer']['user_id'] = $this->Session->read('User.id');
     if($this->Answer->save($this->request->data)) {
       $this->redirect('/questions'); 
     } else {
       $this->Session->setFlash('Could not save answer');
       $this->redirect(array('controller' => 'questions', 'action' => 'view', 
-          $this->request->data['Answer']['question_id']); 
+          $this->request->data['Answer']['question_id'])); 
     }
 
   }
